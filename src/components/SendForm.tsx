8db03e504650288dd6b76eda0b6677bc1439c7be
yujuiting <yujuiting@me.com>
2021-04-10 03:29:21 +0800

    remove unused variable

--- src/components/SendForm.tsx
+++ src/components/SendForm.tsx
@@ -39,7 +39,7 @@ export default function SendForm() {
 
   const {
     handleSubmit,
-    formState: { errors, isValid, isDirty },
+    formState: { isValid, isDirty },
     control,
   } = useForm<FormData>({ mode: "onBlur" });
 
